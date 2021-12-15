03c092a688115ffb55cb2175a9b0751175b6e279
Ken Shibata <kenxshibata@gmail.com>
2021-12-15 11:03:32 -0500

    service worker

---
+++ summative/gulpfile.js
@@ -0,0 +1,72 @@
+const gulp = require("gulp");
+const clean = require("gulp-clean");
+const shell = require("gulp-shell");
+const workbox = require("workbox-build");
+
+gulp.task("clean", () => gulp.src("public", { read: false, allowEmpty: true }).pipe(clean()));
+gulp.task("hugo-build", shell.task(["hugo"]));
+gulp.task("generate-service-worker", () => {
+    return workbox.generateSW({
+        cacheId: "nyiyui-mpm2dp-0a-summative",
+        globDirectory: "./public",
+        globPatterns: [
+            "**/*.{css,js,eot,ttf,woff,woff2,otf}"
+        ],
+        swDest: "./public/sw.js",
+        modifyURLPrefix: {
+            "": "/"
+        },
+        clientsClaim: true,
+        skipWaiting: true,
+        ignoreURLParametersMatching: [/./],
+        maximumFileSizeToCacheInBytes: 50 * 1024 * 1024,
+        runtimeCaching: [
+            {
+                urlPattern: /(?:\/)$/,
+                handler: "StaleWhileRevalidate",
+                options: {
+                    cacheName: "html",
+                    expiration: {
+                        maxAgeSeconds: 60 * 60 * 24 * 7,
+                    },
+                },
+            },
+            {
+                urlPattern: /\.(?:png|jpg|jpeg|gif|bmp|webp|svg|ico)$/,
+                handler: "CacheFirst",
+                options: {
+                    cacheName: "img",
+                    expiration: {
+                        maxEntries: 1000,
+                        maxAgeSeconds: 60 * 60 * 24 * 365,
+                    },
+                },
+            },
+            {
+                urlPattern: /\.(?:mp3|wav|m4a)$/,
+                handler: "CacheFirst",
+                options: {
+                    cacheName: "audio",
+                    expiration: {
+                        maxEntries: 1000,
+                        maxAgeSeconds: 60 * 60 * 24 * 365,
+                    },
+                },
+            },
+            {
+                urlPattern: /\.(?:m4v|mpg|avi)$/,
+                handler: "CacheFirst",
+                options: {
+                    cacheName: "vid",
+                    expiration: {
+                        maxEntries: 1000,
+                        maxAgeSeconds: 60 * 60 * 24 * 365,
+                    },
+                },
+            }
+        ],
+    });
+});
+
+
+gulp.task("build", gulp.series("clean", "hugo-build", "generate-service-worker"));
