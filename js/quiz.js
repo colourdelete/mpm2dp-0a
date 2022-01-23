75081ec8c952bdfc5201143a60276683fd7136ef
nyiyui <nyiyui@users.noreply.github.com>
2022-01-23 09:36:03 +0000

    deploy: 7585d61eef2ef39dbadb43793043e70a46d60f58

--- js/quiz.js
+++ js/quiz.js
@@ -24,7 +24,9 @@ function quiz(id, i, ans, page) {
   elem.style.width = '100%';
   const handler = async () => {
     const correct = (await digestMessage(ie.value)) === ans;
-    se.innerText = (correct ? "✓" : "✗") + (saved ? "remembered" : "");
+    se.innerText = (saved ? "remembered" : "");
+    se.classList.toggle('ok', correct);
+    se.classList.toggle('nok', !correct);
     if (!correct) {
       se.appendChild(elem);
     } else {
@@ -60,4 +62,37 @@ function quizzes(es, target, id, page) {
   };
 }
 
-export { quiz, quizzes, digestMessage };
+function metaQuiz(id, depIds, depDigests, page) {
+  const next = document.getElementById(`metaquiz-${id}-next`);
+  const a = document.getElementById(`metaquiz-${id}-a`);
+  const target = document.getElementById(`metaquiz-${id}-total`);
+  const handler = async () => {
+    let allCorrect = true;
+    let total = '';
+    for (let [i, depId] of depIds.entries()) {
+      const depDigest = depDigests[i];
+      const e = document.getElementById(`${page}-quiz-${depId}-s${i}`);
+      const status = document.getElementById(`metaquiz-${id}-s${i}`);
+      const key = `${page}-quiz-${depId}-total`;
+      const value = localStorage.getItem(key);
+      const correct = value === depDigest;
+      status.classList.toggle('ok', correct);
+      status.classList.toggle('nok', !correct);
+      total += value;
+      allCorrect = allCorrect && correct;
+    }
+    const digest = await digestMessage(total);
+    target.innerText = digest;
+    next.style.display = allCorrect ? 'block' : 'none';
+    a.href = `/mpm2dp-0a/redirect/${digest}`;
+  };
+  const reloadBtn = document.getElementById(`metaquiz-${id}-reload`);
+  reloadBtn.addEventListener('click', handler);
+  for (let depId of depIds) {
+    const es = document.querySelectorAll(`#quiz-${depId} input`);
+    es.forEach((e) => e.addEventListener('input', handler));
+  }
+  handler();
+}
+
+export { quiz, quizzes, digestMessage, metaQuiz };
